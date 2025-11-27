6fc30ebae53a803089ed35fc1005c84e25a293c7
GiovaneGuimaraes <gioguima24@gmail.com>
2025-11-27 12:37:44 -0300

    feat: add routes to register user and login

---
+++ packages/db/src/index.ts
@@ -0,0 +1,14 @@
+import { sequelize } from "./db";
+import * as models from "./models";
+
+(async () => {
+  try {
+    models;
+    await sequelize.sync({ alter: true });
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
+
+export { sequelize };
+export * from "./models";
